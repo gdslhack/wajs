319711f2b669d69ccdbe27b88fda05a558714882
gdslhack <34924797+gdslhack@users.noreply.github.com>
2021-02-10 14:25:53 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const { Client, MessageMedia } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http')
 const fs = require('fs');
+const { phoneNumberFormatter } = require('./helpers/formatter');
+const fileUpload = require ('express-fileupload');
+const port = process.env.PORT || 8000;
+const axios = require ('axios');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+        extended: true
+}));
+app.use(fileUpload({
+        debug: true
+}));
 
 const SESSION_FILE_PATH = './whatsapp-session.json';
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
+app.get('/', (req, res) => {
+        res.sendFile('index.html', { root: __dirname });
+});
 
 const client = new Client({
   restartOnAuthFail: true,
@@ -23,16 +46,8 @@ const client = new Client({
       '--disable-gpu'
     ],
   },
+
   session: sessionCfg
-});
-
-client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
-    qrcode.generate(qr);
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
 });
 
 client.on('message', msg => {
@@ -42,7 +57,7 @@ client.on('message', msg => {
     msg.reply('selamat pagi');
   } else if (msg.body == '!groups') {
     client.getChats().then(chats => {
-      const groups = chats.filter(chat => chat.isGroup);
+              const groups = chats.filter(chat => chat.isGroup);
 
       if (groups.length == 0) {
         msg.reply('You have no group yet.');
@@ -56,3 +71,112 @@ client.on('message', msg => {
       }
     });
   }
+});
+
+client.initialize();
+
+// Socket IO
+io.on('connection', function(socket) {
+  socket.emit('message', 'Connecting...');});
+
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr, (err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code received, scan please!');
+    });
+  });
+
+client.on('ready', () => {
+        socket.emit('ready', 'Whatsapp is ready!');
+        socket.emit('message', 'Whatsapp is ready!');
+  });
+
+client.on('authenticated', (session) => {
+         socket.emit('authenticated', 'Whatsapp is authenticated');
+         socket.emit('authenticated', 'Whatsapp is authenticated');
+    console.log('AUTHENTICATED', session);
+    sessionCfg=session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+const checkRegisteredNumber = async function(number) {
+     const isRegistered = await client.isRegisteredUser(number);
+     return isRegistered;
+}
+// Send message
+        app.post('/send-message', [
+        body('number').notEmpty(),
+        body('message').notEmpty(),
+], async (req, res) => {
+        const error = validationResult(req).formatWith(({ msg }) => {
+        return msg;
+        });
+
+        if(!errors.isEmpty()) {
+        return res.status(422).json({
+                status: false,
+                message: errors.mapped()
+        });
+        }
+        const number = phoneNumberFormatter(req.body.number);
+        const message = req.body.message;
+
+        const isRegisteredNumber = await checkRegisteredNumber(number);
+
+        if (!isRegisteredNumber) {
+                return res.status(422).json({
+                status: false,
+                message: 'The Numer is not registered'
+        });
+        }
+        client.sendMessage(number, message).then(response => {
+          res.status(200).json({
+                status: true,
+                response: response
+                });
+        }).catch(err => {
+                res.status(500).json({
+                status: false,
+                response: err
+    });
+    });
+  });
+
+//send media
+         app.post('send-media', async (req, res) => {
+        const number = phoneNumberFormatter(req.body.number);
+        const caption = req.body.caption;
+        const fileUrl = req.body.file;
+
+        // const media = MessageMedia.fromFilePath('./img.png');
+        //  const file = req.files.file;
+        //  const media = new MessegaMedia(file.mimetype, file.data.toString('base64'), file.name);
+        let mimetype;
+        const attachment = await axios.get(fileUrl, { responseType: 'arraybuffer' }).then(response => {
+          mimetype = response.headers['content-type'];
+          return response.data.toString('base64');
+  });
+
+         const media = new MessegaMedia(mimetype, attachment, 'Media');
+
+        client.sendMessage(number, media, { caption: caption }).then(response => {
+          res.status(200).json({
+                status: true,
+                response: response
+                });
+        }).catch(err => {
+                res.status(500).json({
+                status: false,
+                response: err
+        });
+     });
+});
+
+server.listen(port, function(){
+        console.log('App Running on *: ' + port);
+});
